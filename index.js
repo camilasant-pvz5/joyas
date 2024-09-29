d7ebbb9b67286a678b6ce7f515843d35d2e1c1c9
Min. <154395301+camilasant-pvz5@users.noreply.github.com>
2024-09-29 17:05:46 -0300

    add get anterior y nuevo

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
 
+app.get('/inventario', async (req, res) => {
+    try {
+        const queryStrings = req.query;
+        const inventario = await obtenerJoyas(queryStrings);
+        res.json(inventario); 
+    } catch (error) {
+        console.error("Error al obtener las joyas:", error);
+        res.status(500).json({ error: "Error al obtener las joyas" });
+    }
+});
+
 app.get('/inventario/filtros', async (req, res) => {
     try {
         const queryStrings = req.query;
