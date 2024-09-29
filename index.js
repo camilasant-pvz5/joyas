36c947873eda97384f99a5e973cbf9877ed35486
Min. <154395301+camilasant-pvz5@users.noreply.github.com>
2024-09-29 17:07:37 -0300

    obtener filtros

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { obtenerJoyas } = require('./consulta');
+const { obtenerJoyas, obtenerJoyasFiltro} = require('./consulta');
 
 const app = express();
 
