d9887a91149da105047c9288a8cba0971beff119
Min. <154395301+camilasant-pvz5@users.noreply.github.com>
2024-09-29 18:28:46 -0300

    se arregla puerto 3000

--- consulta.js
+++ consulta.js
@@ -10,24 +10,22 @@ const pool = new Pool({
 });
 
 const prepararHateoas = (inventario) => {
-  const results = inventario.map((j) => {
+  const results = inventario.map((m) => {
     return {
       name: m.nombre,
       href: `/inventario/${m.id}`,
     };
-  }).slice(0, 4);  
+  }).slice(0, 4);
   const total = inventario.length;
-  return {
+  const HATEOAS = {
     total,
     results
   }
-  return HATEOAS
-  ;
+  return HATEOAS;
 };
 
 const obtenerJoyas = async ({ limits = 10, order_by = "id_ASC", page = 0 }) => {
-
-  const { columna, orden } = order_by.split("_");
+  const [columna, orden] = order_by.split("_");
   const offset = page * limits;
   let consulta = format(
     "SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s",
@@ -41,7 +39,6 @@ const obtenerJoyas = async ({ limits = 10, order_by = "id_ASC", page = 0 }) => {
   return { total: 100, results: inventario };
 };
 
-
 const obtenerJoyasFiltro = async ({ precio_max, precio_min, categoria, metal }) => {
   let filtros = [];
   const values = [];
@@ -50,7 +47,7 @@ const obtenerJoyasFiltro = async ({ precio_max, precio_min, categoria, metal }) 
     const { length } = filtros;
     filtros.push(`${campo} ${comparador} $${length + 1}`);
   };
-if (precio_max) agregarFiltro('precio', '<=', precio_max);
+  if (precio_max) agregarFiltro('precio', '<=', precio_max);
   if (precio_min) agregarFiltro('precio', '>=', precio_min);
   if (categoria) agregarFiltro('categoria', '=', categoria);
   if (metal) agregarFiltro('metal', '=', metal);
