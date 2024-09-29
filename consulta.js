9ac2c49d4909552dec10de298f80fb2dd1a35263
Min. <154395301+camilasant-pvz5@users.noreply.github.com>
2024-09-29 17:59:26 -0300

    info

--- consulta.js
+++ consulta.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 const format = require('pg-format');
-
 const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
@@ -10,13 +9,26 @@ const pool = new Pool({
   port: 5432,
 });
 
+const prepararHateoas = (inventario) => {
+  const results = inventario.map((j) => {
+    return {
+      name: m.nombre,
+      href: `/inventario/${m.id}`,
+    };
+  }).slice(0, 4);  
+  const total = inventario.length;
+  return {
+    total,
+    results
+  }
+  return HATEOAS
+  ;
+};
 
 const obtenerJoyas = async ({ limits = 10, order_by = "id_ASC", page = 0 }) => {
 
   const { columna, orden } = order_by.split("_");
   const offset = page * limits;
-
-  
   let consulta = format(
     "SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s",
     columna,
@@ -33,16 +45,12 @@ const obtenerJoyas = async ({ limits = 10, order_by = "id_ASC", page = 0 }) => {
 const obtenerJoyasFiltro = async ({ precio_max, precio_min, categoria, metal }) => {
   let filtros = [];
   const values = [];
-
-
   const agregarFiltro = (campo, comparador, valor) => {
     values.push(valor);
     const { length } = filtros;
     filtros.push(`${campo} ${comparador} $${length + 1}`);
   };
-
-  
-  if (precio_max) agregarFiltro('precio', '<=', precio_max);
+if (precio_max) agregarFiltro('precio', '<=', precio_max);
   if (precio_min) agregarFiltro('precio', '>=', precio_min);
   if (categoria) agregarFiltro('categoria', '=', categoria);
   if (metal) agregarFiltro('metal', '=', metal);
@@ -58,5 +66,5 @@ const obtenerJoyasFiltro = async ({ precio_max, precio_min, categoria, metal }) 
   return inventario;
 };
 
-module.exports = { obtenerJoyas, obtenerJoyasFiltro };
+module.exports = { obtenerJoyas, obtenerJoyasFiltro, prepararHateoas };
 
--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-
 const express = require('express');
-const { obtenerJoyas, obtenerJoyasFiltro} = require('./consulta');
-
+const {obtenerJoyas, obtenerJoyasFiltro, prepararHateoas} = require('./consulta');
+const format = require('pg-format');
 const app = express();
-
-
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
-
-
-app.get('/inventario', async (req, res) => {
-    try {
-        const queryStrings = req.query;
-        const inventario = await obtenerJoyas(queryStrings);
-        res.json(inventario); 
-    } catch (error) {
-        console.error("Error al obtener las joyas:", error);
-        res.status(500).json({ error: "Error al obtener las joyas" });
-    }
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-app.get('/inventario/filtros', async (req, res) => {
+app.get('/joyas', async (req, res) => { 
+try {
+    const queryStrings = req.query;
+    const { results: inventario } = await obtenerJoyas(queryStrings); 
+    const HATEOAS = await prepararHateoas(inventario);
+    res.json({ inventario, HATEOAS });
+    
+} catch (error) {
+    console.error("Error al obtener las joyas:", error);
+    res.status(500).json({ error: "Error al obtener las joyas" });
+}}
+);
+
+app.get("/inventario/filtros", async (req, res) => {
     try {
-        const queryStrings = req.query;
-        const inventario = await obtenerJoyasFiltro(queryStrings);
-        res.json(inventario); 
+      const queryStrings = req.query;
+      const inventario = await obtenerJoyasFiltro(queryStrings);
+      res.json(inventario);
     } catch (error) {
-        console.error("Error al obtener las joyas:", error);
-        res.status(500).json({ error: "Error al obtener las joyas" });
-    }
-});
+      console.error("Error al obtener las joyas:", error);
+      res.status(500).json({ error: "Error al obtener las joyas" });
+    }
+  });
+
+
+
